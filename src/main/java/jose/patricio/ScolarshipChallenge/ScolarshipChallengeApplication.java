8366d5f0b5a146f726f9c2cd37a8f798aa62e17d
Jose Patricio Neto <josepatricioneto@hotmail.com>
2023-08-08 06:51:53 -0300

    :tada: First Commit

---
+++ src/main/java/jose/patricio/ScolarshipChallenge/ScolarshipChallengeApplication.java
@@ -0,0 +1,13 @@
+package jose.patricio.ScolarshipChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScolarshipChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScolarshipChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jose/patricio/ScolarshipChallenge/ScolarshipChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package jose.patricio.ScolarshipChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScolarshipChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
