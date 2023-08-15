9039975b5a38e602bfa63b4f2ab72e931148d985
Jose Patricio Neto <josepatricioneto@hotmail.com>
2023-08-15 05:38:30 -0300

    :sparkles: feat: Business Rule: Each squad should have a maximum of 5 students

--- src/main/java/jose/patricio/ScolarshipChallenge/services/StudentServiceImpl.java
+++ src/main/java/jose/patricio/ScolarshipChallenge/services/StudentServiceImpl.java
@@ -2,11 +2,13 @@ package jose.patricio.ScolarshipChallenge.services;
 
 import jose.patricio.ScolarshipChallenge.entities.ClassEntity;
 import jose.patricio.ScolarshipChallenge.entities.ClassStatus;
+import jose.patricio.ScolarshipChallenge.entities.SquadEntity;
 import jose.patricio.ScolarshipChallenge.exceptions.ClassArgumentException;
 import jose.patricio.ScolarshipChallenge.exceptions.IdNotFoundException;
 import jose.patricio.ScolarshipChallenge.dtos.StudentRecord;
 import jose.patricio.ScolarshipChallenge.entities.StudentEntity;
 import jose.patricio.ScolarshipChallenge.repositories.ClassRepository;
+import jose.patricio.ScolarshipChallenge.repositories.SquadRepository;
 import jose.patricio.ScolarshipChallenge.repositories.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,13 +18,15 @@ import java.util.List;
 public class StudentServiceImpl implements StudentService{
 
     private static final String IDNOTFOUND = "Student Id not found";
-    private StudentRepository studentRepository;
-    private ClassRepository classRepository;
+    private final StudentRepository studentRepository;
+    private final ClassRepository classRepository;
+    private final SquadRepository squadRepository;
 
     @Autowired
-    public StudentServiceImpl(StudentRepository studentRepository, ClassRepository classRepository) {
+    public StudentServiceImpl(StudentRepository studentRepository, ClassRepository classRepository, SquadRepository squadRepository) {
         this.studentRepository = studentRepository;
         this.classRepository = classRepository;
+        this.squadRepository = squadRepository;
     }
 
     @Override
@@ -42,17 +46,9 @@ public class StudentServiceImpl implements StudentService{
     @Override
     public StudentRecord createStudent(StudentRecord studentRecordToCreate) {
 
-        ClassEntity classEntity1;
+        ifClassNotNullCheckStatus(studentRecordToCreate);
 
-        if (studentRecordToCreate.classEntity() != null) {
-             classEntity1 = classRepository.findById(studentRecordToCreate.classEntity().getId())
-                    .orElseThrow(() -> new IdNotFoundException("Class Id not found"));
-            if (!classEntity1.getStatus().equals(ClassStatus.WAITING)) {
-                throw new ClassArgumentException("Students can only be added to a class in the 'waiting' status.");
-            }
-        }
-
-
+        ifSquadNotNullCheckSize(studentRecordToCreate);
 
         StudentEntity studentEntity = mapToStudentEntity(studentRecordToCreate);
         studentEntity = studentRepository.save(studentEntity);
@@ -62,6 +58,10 @@ public class StudentServiceImpl implements StudentService{
 
     @Override
     public StudentRecord updateStudent(Long id, StudentRecord updatedStudentRecord) {
+
+        ifSquadNotNullCheckSize(updatedStudentRecord);
+        ifClassNotNullCheckStatus(updatedStudentRecord);
+
         return studentRepository.findById(id)
                 .map(existingStudentEntity -> updateAndSaveStudentEntity(existingStudentEntity, updatedStudentRecord))
                 .map(this::mapToStudentRecord)
@@ -109,6 +109,29 @@ public class StudentServiceImpl implements StudentService{
         existingStudentEntity.setClassEntity(updatedStudentRecord.classEntity());
 
         return studentRepository.save(existingStudentEntity);
+
+    }
+    private void ifClassNotNullCheckStatus(StudentRecord studentRecord) {
+
+        if (studentRecord.classEntity() != null) {
+            ClassEntity classEntity = classRepository.findById(studentRecord.classEntity().getId())
+                    .orElseThrow(() -> new IdNotFoundException("Class Id not found"));
+            if (!classEntity.getStatus().equals(ClassStatus.WAITING)) {
+                throw new ClassArgumentException("Students can only be added to a class in the 'waiting' status.");
+            }
+        }
+    }
+
+    private void ifSquadNotNullCheckSize(StudentRecord studentRecord) {
+
+        if (studentRecord.squadEntity() != null) {
+            SquadEntity squadEntity = squadRepository.findById(studentRecord.squadEntity().getId())
+                    .orElseThrow(() -> new IdNotFoundException("Squad Id not found"));
+            if(squadEntity.getStudentEntities().size() > 5) {
+                throw new ClassArgumentException("Squads must have at most 5 Students");
+            }
+        }
+
 
     }
 
