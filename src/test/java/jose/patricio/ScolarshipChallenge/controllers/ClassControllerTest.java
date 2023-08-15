dc88b52cc6d0897acfee42c0d6f205bbd5204297
Jose Patricio Neto <josepatricioneto@hotmail.com>
2023-08-15 02:40:57 -0300

    :sparkles: feat: ClassControler Test http

--- src/test/java/jose/patricio/ScolarshipChallenge/controllers/ClassControllerTest.java
+++ src/test/java/jose/patricio/ScolarshipChallenge/controllers/ClassControllerTest.java
@@ -1,5 +1,6 @@
 package jose.patricio.ScolarshipChallenge.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jose.patricio.ScolarshipChallenge.dtos.ClassRecord;
 import jose.patricio.ScolarshipChallenge.entities.ClassStatus;
 import jose.patricio.ScolarshipChallenge.services.ClassService;
@@ -8,53 +9,54 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
 
+@WebMvcTest(controllers = ClassController.class)
 public class ClassControllerTest {
 
-    @Mock
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
     private ClassService classService;
 
-    @InjectMocks
-    private ClassController classController;
-
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-    }
-
     @Test
-    void testGetAllClasses() {
+    void testGetAllClasses() throws Exception {
         List<ClassRecord> classRecords = Collections.singletonList(
                 new ClassRecord(1L, "Spring Boot", ClassStatus.WAITING, new Date(), new Date(), null, null, null)
         );
         when(classService.getAllClasses()).thenReturn(classRecords);
 
-        ResponseEntity<List<ClassRecord>> responseEntity = classController.getAllClasses();
-
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-        assertEquals(classRecords, responseEntity.getBody());
+        mockMvc.perform(MockMvcRequestBuilders.get("/v1/classes"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(objectMapper.writeValueAsString(classRecords)));
     }
 
     @Test
-    void testGetClassById() {
+    void testGetClassById() throws Exception {
         ClassRecord classRecord = new ClassRecord(
                 1L, "Spring Boot", ClassStatus.WAITING, new Date(), new Date(), null, null, null
         );
         when(classService.getClassById(1L)).thenReturn(classRecord);
 
-        ResponseEntity<ClassRecord> responseEntity = classController.getClassById(1L);
-
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-        assertEquals(classRecord, responseEntity.getBody());
+        mockMvc.perform(MockMvcRequestBuilders.get("/tv1/classes/1"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(objectMapper.writeValueAsString(classRecord)));
     }
-
-
 }
